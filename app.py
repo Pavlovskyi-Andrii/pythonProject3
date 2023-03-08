e5dfdc7c3c21e43373d4ebcd3814b2f8325cc81b
Andrey Pavlovsky <88763822+Pavlovskyi-Andrii@users.noreply.github.com>
2023-03-08 23:59:30 +0200

    DZ 40

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask,render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 #from forms import LoginForm
+from sqlalchemy.sql import func
+
+
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///blog.db"
@@ -31,17 +34,6 @@ class Tasks(db.Model):                           # создаем колонки
         return "<Tasks %r>" % self.id
 
 
-class Contact(db.Model):
-    id = db.Column(db.Integer, primary_key=True) # создаем колонки таблицы базы данных
-    hireUsFormFirstName = db.Column(db.String(20), nullable=False)
-    hireUsFormLasttName = db.Column(db.String(29), nullable=False)
-    hireUsFormWorkEmail = db.Column(db.String(50), nullable=False)
-    hireUsFormPhone = db.Column(db.String(20),nullable=False)
-    hireUsFormDetails = db.Column(db.Text, nullable=False)
-    date = db.Column(db.DateTime, default= datetime.utcnow)
-
-    def __repr__(self):                         #указіваем какое значение возвращаем с базы данных
-        return "<Contact %r>" % self.id
 
 
 
@@ -63,9 +55,9 @@ def form():
 def about():
     return render_template("about.html")
 
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
+
+
+
 
 
 
@@ -77,7 +69,6 @@ def user(name,id):
 @app.route("/<string:admin>/<string:login>/<int:id>")
 def admin(admin,login,id):
     return "Welcome: " +admin+ " - "+login+"-" +str(id)
-
 
 
 @app.route("/blog/news")
@@ -95,7 +86,7 @@ def posts():
 
 @app.route("/posts/<int:id>/del")
 def post_delete(id):
-    article=Article.query.get_or_404(id)
+    article=Article.Session.get_or_404(id)
     try:
         db.session.delete(article)
         db.session.commit()
@@ -109,12 +100,9 @@ def post_delete(id):
 
 
 
-
-
-
 @app.route("/posts/<int:id>")
 def post_detail(id):
-    article=Article.query.get(id)
+    article=Article.Session.get(id)
     return render_template("post_detail.html", article=article)
 
 
@@ -123,7 +111,7 @@ def post_detail(id):
 
 @app.route("/posts/<int:id>/update", methods=["POST","GET"])
 def post_update(id):
-    article=Article.query.get(id)
+    article=Article.Session.getid
     if request.method == "POST":
         article.title = request.form["title"]
         article.intro = request.form["intro"]
@@ -154,15 +142,12 @@ def create_article():
         try:
             db.session.add(article)
             db.session.commit()
-            return redirect("/posts")
+            return redirect("/")
 
         except:
             return ("При додаванні виникла помилка ")
     else:
         return  render_template("create-article.html")
-
-
-#_____________________________________________________________________________
 
 
 
@@ -195,7 +180,7 @@ def pos_test():
 
 @app.route("/tasks/<int:id>")
 def test_ditail(id):
-    tasks = Tasks.query.get(id)
+    tasks = Tasks.Session.get(id)
 
     return render_template('tasks_detail.html', task=tasks)
 
@@ -203,24 +188,38 @@ def test_ditail(id):
 
 
 
-#_____________________________________________________________________________
-"""
-@app.route("/contact", methods=["POST","GET"])
-def contact():
-    if request.method == "POST":
-        hireUsFormFirstName = request.form["hireUsFormFirstName"]
-        hireUsFormLasttName = request.form["hireUsFormLasttName"]
-        hireUsFormWorkEmail = request.form["hireUsFormWorkEmail"]
-        hireUsFormPhone = request.form["hireUsFormPhone"]
-        hireUsFormDetails = request.form["hireUsFormDetails"]
+#____________________________Contact(db.Model):_________________________________________________
 
-        contact= Contact(hireUsFormFirstName = hireUsFormFirstName, hireUsFormLasttName = hireUsFormLasttName, hireUsFormWorkEmail = hireUsFormWorkEmail, hireUsFormPhone=hireUsFormPhone, hireUsFormDetails=hireUsFormDetails)
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True) # создаем колонки таблицы базы данных
+    hireUsFormNameFirstName = db.Column(db.String(20), nullable=False)
+    hireUsFormNameLastName = db.Column(db.String(29), nullable=False)
+    hireUsFormNameWorkEmail = db.Column(db.String(50), nullable=False)
+    hireUsFormNamePhone = db.Column(db.String(20),nullable=False)
+    hireUsFormNameDetails = db.Column(db.Text, nullable=False)
+
+    date = db.Column(db.DateTime, default= datetime.utcnow)
+
+    def __repr__(self):                         #указіваем какое значение возвращаем с базы данных
+        return "<Contact %r>" % self.id
+
+
+@app.route("/contact", methods=["POST","GET"])
+def contact_post():
+    if request.method == "POST":
+        hireUsFormNameFirstName = request.form["hireUsFormNameFirstName"]
+        hireUsFormNameLastName = request.form["hireUsFormNameLastName"]
+        hireUsFormNameWorkEmail = request.form["hireUsFormNameWorkEmail"]
+        hireUsFormNamePhone = request.form["hireUsFormNamePhone"]
+        hireUsFormNameDetails = request.form["hireUsFormNameDetails"]
+
+        contact = Contact(hireUsFormNameFirstName = hireUsFormNameFirstName, hireUsFormNameLastName = hireUsFormNameLastName, hireUsFormNameWorkEmail = hireUsFormNameWorkEmail, hireUsFormNamePhone=hireUsFormNamePhone, hireUsFormNameDetails=hireUsFormNameDetails)
 
 
         try:
             db.session.add(contact)
             db.session.commit()
-            return redirect("/")
+            return redirect("/contact")
 
         except:
             return ("При додаванні виникла помилка ")
@@ -229,42 +228,34 @@ def contact():
 
 
 @app.route("/cont")
-def cont_test():
-    contact = Contact.query.order_by(Contact.date.desc()).all()
-    return render_template('cont.html', contact=contact)
+def prover_kontact():
+    contact = Contact.query.order_by(Contact.date).all()
+    return render_template("cont.html",contact=contact)
+@app.route("/cont")
+# def cont_test():
+#     contact = Contact.query.order_by(Contact.date.desc()).all()
+#     return render_template('cont.html', contact=contact)
 
 
 @app.route("/cont/<int:id>")
 def cont_ditail(id):
     contact = Contact.query.get(id)
+    return render_template('contact_detail.html', contact=contact)
 
-    return render_template('contact_detail.html', contac=contact)"""
+
+
+
+
+
 
 #_____________________________________________________________________________
 
 
 
 
-"""@app.route("/login",methods=["POST","GET"])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('profile'))
 
-    form = LoginForm()
-    return render_tamplate("login.html", menu =db.getMenu(), title = "Avtoriza",)"""
-    # if request.method == "POST":
-    #     user = dbase.getUserByEmail(request.form['email']
-    #     if user and check_password_hash(user['psw'], request.form['psw']):
-    #         userlogin = UserLogin().create(user)
-    #         rm =True if request.form.get('remainme') else False
-    #         Login_user(userlogin, remember=rm)
-    #         return redirect(request.args.get("next") or url_for("profile"))
-    #     flash("НЕ верная пара логин пароль", "error")
-    #
-    # return render_template("Login.html", menu=dbase.getMenu(), title="ABTopn3auna")
-
-
-
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True)
